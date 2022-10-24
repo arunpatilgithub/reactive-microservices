36bce31035925781674e76192fb8c62e09fdc781
arunpatil <arunpatilmailbox@gmail.com>
2022-10-24 09:29:09 -0600

    Added openapi support

--- api/src/main/java/com/ap/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/ap/api/composite/product/ProductCompositeService.java
@@ -1,5 +1,8 @@
 package com.ap.api.composite.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,6 +14,19 @@ public interface ProductCompositeService {
      * @param productId
      * @return the composite product info, if found, else null
      */
+    @Operation(
+            summary = "${api.product-composite.get-composite-product.description}",
+            description = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+                            @ApiResponse(responseCode = "400",
+                                         description = "Bad Request, invalid format of the request.See response " +
+                                                "message for more information."),
+                            @ApiResponse(responseCode = "404",
+                                         description = "Not found, the specified id does not exist."),
+                            @ApiResponse(responseCode = "422",
+                                         description = "Unprocessable entity, input parameters caused the processing " +
+                                                "to fails. See response message for more information.")
+    })
     @GetMapping(
         value    = "/product-composite/{productId}",
         produces = "application/json")
