67f6c7ca8f406b2b5e8349f144fd6539561af5d1
arunpatil <arunpatilmailbox@gmail.com>
2022-10-24 16:53:33 -0600

    Added jpa entity and repository and also junit5.

---
+++ core/recommendation-service/src/main/java/com/ap/microservices/core/recommendation/persistence/RecommendationRepository.java
@@ -0,0 +1,9 @@
+package com.ap.microservices.core.recommendation.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+    List<RecommendationEntity> findAllByProductId(int productId);
+}
